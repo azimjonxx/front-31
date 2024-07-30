ef7703b086582b8a750992afd57a6a2b04a35e26
azimjonxatamov <azimjonxatamov6@gmail.com>
2024-07-30 15:20:22 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let body = document.querySelector('body');
+let text = document.querySelector('p');
+let a = prompt('enter body backgroundcolor');
+let b = prompt('enter text color');
+body.style.backgroundColor = a;
+text.style.color = b;
